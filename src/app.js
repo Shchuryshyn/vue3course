f36d1781fdfe6273a4b82d80f1a49b7130721343
alexs <alexshchuryshyn@gmail.com>
2022-05-09 22:15:13 +0200

    second commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 const App = {
 data(){
  return{
-   counter: 5,
-     title: "Cчетчик"
+     title: "Notes",
+     placeholderString: "Write something here...",
+     inputValue: '',
+     notes: ["Note1","Note2","Note154"]
   }
+ },
+methods: {
+
+inputChangeHandler(event) {
+    this.inputValue = event.target.value
+},
+addNewNote(){
+    this.notes.push( this.inputValue)
+    this.inputValue = ''
+}
  }
+
 }
 
 Vue.createApp(App).mount('#app')
